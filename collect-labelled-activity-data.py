bc75075ba7182415b081c7c0b06a05d73ee675b7
Todd <rtoddmorse@gmail.com>
2016-10-16 14:28:07 -0400

    Added group ID

--- collect-labelled-activity-data.py
+++ collect-labelled-activity-data.py
@@ -6,7 +6,7 @@ Assignment A0 : Data Collection
 
 @author: cs390mb
 
-This Python script receives incoming labelled accelerometer data through 
+This Python script receives incoming labelled accelerometer data through
 the server and saves it in .csv format to disk.
 
 """
@@ -17,18 +17,18 @@ import json
 import numpy as np
 
 # TODO: Replace the string with your user ID
-user_id = ""
+user_id = "be.af.9a.d0.e9.3f.e3.db.8f.94"
 
 count = 0
 
 '''
     This socket is used to send data back through the data collection server.
-    It is used to complete the authentication. It may also be used to send 
-    data or notifications back to the phone, but we will not be using that 
+    It is used to complete the authentication. It may also be used to send
+    data or notifications back to the phone, but we will not be using that
     functionality in this assignment.
 '''
 send_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-send_socket.connect(("none.cs.umass.edu", 9999))    
+send_socket.connect(("none.cs.umass.edu", 9999))
 
 #################   Server Connection Code  ####################
 
@@ -47,7 +47,7 @@ msg_acknowledge_id = "ACK"
 def authenticate(sock):
     """
     Authenticates the user by performing a handshake with the data collection server.
-    
+
     If it fails, it will raise an appropriate exception.
     """
     message = sock.recv(256).strip()
@@ -64,13 +64,13 @@ def authenticate(sock):
     except:
         print("Authentication failed!")
         raise Exception("Wait timed out. Failed to receive authentication response from server.")
-        
+
     if (message.startswith(msg_acknowledge_id)):
         ack_id = message.split(",")[1]
     else:
         print("Authentication failed!")
         raise Exception("Expected message with prefix '{}' from server, received {}".format(msg_acknowledge_id, message))
-    
+
     if (ack_id == user_id):
         print("Authentication successful.")
         sys.stdout.flush()
@@ -83,18 +83,18 @@ try:
     print("Authenticating user for receiving data...")
     sys.stdout.flush()
     authenticate(receive_socket)
-    
+
     print("Authenticating user for sending data...")
     sys.stdout.flush()
     authenticate(send_socket)
-    
+
     print("Successfully connected to the server! Waiting for incoming data...")
     sys.stdout.flush()
-        
+
     previous_json = ''
-    
+
     labelled_data = []
-        
+
     while True:
         try:
             message = receive_socket.recv(1024).strip()
@@ -116,9 +116,9 @@ try:
                     z = data['data']['z']
                     label = data['data']['label']
                     labelled_data.append([t, x, y, z, label])
-                
+
             sys.stdout.flush()
-        except KeyboardInterrupt: 
+        except KeyboardInterrupt:
             # occurs when the user presses Ctrl-C
             print("User Interrupt. Quitting...")
             break
@@ -126,10 +126,10 @@ try:
             # ignore exceptions, such as parsing the json
             # if a connection timeout occurs, also ignore and try again. Use Ctrl-C to stop
             # but make sure the error is displayed so we know what's going on
-            if (e.message != "timed out"):  # ignore timeout exceptions completely       
+            if (e.message != "timed out"):  # ignore timeout exceptions completely
                 print(e)
             pass
-except KeyboardInterrupt: 
+except KeyboardInterrupt:
     # occurs when the user presses Ctrl-C
     print("User Interrupt. Saving labelled data...")
     labelled_data = np.asarray(labelled_data)
@@ -138,7 +138,7 @@ finally:
     print >>sys.stderr, 'closing socket for receiving data'
     receive_socket.shutdown(socket.SHUT_RDWR)
     receive_socket.close()
-    
+
     print >>sys.stderr, 'closing socket for sending data'
     send_socket.shutdown(socket.SHUT_RDWR)
     send_socket.close()
